502d0e4f15b410fd0b2aaa615d9d649214b029b4
lapeko <lapeko88@gmail.com>
2024-10-17 22:24:52 +0200

    make landing page with styles

--- main-project/marketing/src/components/Landing.tsx
+++ main-project/marketing/src/components/Landing.tsx
@@ -1,5 +1,15 @@
+import {Link} from "react-router-dom";
+
 export const Landing = () => (
-    <h1 className="text-3xl font-bold underline">
-      Hello world 123!
-    </h1>
+    <div className="container mx-auto px-4 md:px-6 lg:px-8 pt-4">
+        <h1 className="text-center text-2xl font-bold mb-4">Landing page</h1>
+        <p className="mb-4 text-gray-800 font-bold">Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, debitis eaque eligendi error illum impedit odit
+            quas sint sit ullam. Amet est maiores minima non nostrum, pariatur temporibus. Ab animi, beatae commodi
+            culpa ipsam nostrum nulla temporibus voluptas. A ad commodi ex illo in laboriosam laborum mollitia nam
+            nobis, totam!
+        </p>
+        <div className="flex justify-center mb-4">
+            <Link to="/pricing" className="text-center px-4 py-2 bg-indigo-500 text-white rounded-md">Prices</Link>
+        </div>
+    </div>
 )
