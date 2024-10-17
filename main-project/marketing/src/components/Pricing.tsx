b72c00dcf084da6502ca50ac279b75ce30fcc053
lapeko <lapeko88@gmail.com>
2024-10-17 22:28:57 +0200

    simple layout for pricing component

--- main-project/marketing/src/components/Pricing.tsx
+++ main-project/marketing/src/components/Pricing.tsx
@@ -1,5 +1,5 @@
 export const Pricing = () => (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="container mx-auto px-4 md:px-6 lg:px-8 pt-4">
+        <h1 className="text-center mb-4 text-2xl font-bold">Pricing</h1>
+    </div>
 );
